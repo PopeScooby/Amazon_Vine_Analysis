7f933669f11d4d1779777dc7e34f29656ec3543b
Pope Scooby <PopeScooby@gmail.com>
2022-04-24 11:24:25 -0500

    deliv 3 images

--- vine_review_analysis.sql
+++ vine_review_analysis.sql
@@ -2,11 +2,11 @@ DROP TABLE Deliverable2_S01_TotalVotes;
 DROP TABLE Deliverable2_S02_HelpfulPct;
 DROP TABLE Deliverable2_S03_IsVine;
 DROP TABLE Deliverable2_S04_NotVine;
+DROP TABLE Deliverable2_S05_Analysis;
 
 SELECT * 
 INTO Deliverable2_S01_TotalVotes
 FROM vine_table WHERE total_votes >= 20;
-
 
 SELECT * 
 INTO Deliverable2_S02_HelpfulPct
@@ -20,39 +20,30 @@ SELECT *
 INTO Deliverable2_S04_NotVine
 FROM Deliverable2_S02_HelpfulPct WHERE vine = 'N';
 
+SELECT Count_TotalReviews, Count_All5Star, 
+		Count_Vine, Count_Vine5Star, 
+		Cast(Count_Vine5Star / Count_All5Star as numeric(10,4)) as Pct_Vine5Star_All, 
+		Cast(Count_Vine5Star / Count_Vine as numeric(10,4)) as Pct_Vine5Star_Self,
+		Count_NonVine, Count_NonVine5Star, 
+		Cast(Count_NonVine5Star / Count_All5Star as numeric(10,4)) as Pct_NonVine5Star_All, 
+		Cast(Count_NonVine5Star / Count_NonVine as numeric(10,4)) as Pct_NonVine5Star_Self
+INTO	Deliverable2_S05_Analysis
+FROM (
+	SELECT cast(COUNT(review_id) as float) as Count_TotalReviews, cast(SUM(CASE WHEN star_rating = 5 THEN 1 ELSE 0 END) as float) as Count_All5Star
+	FROM Deliverable2_S02_HelpfulPct
+	) as ii CROSS JOIN (
+	SELECT cast(COUNT(review_id) as float) as Count_Vine, SUM(CASE WHEN star_rating = 5 THEN 1 ELSE 0 END) as Count_Vine5Star
+	FROM Deliverable2_S03_IsVine
+	) as iii CROSS JOIN (
+	SELECT cast(COUNT(review_id) as float) as Count_NonVine, SUM(CASE WHEN star_rating = 5 THEN 1 ELSE 0 END) as Count_NonVine5Star
+	FROM Deliverable2_S04_NotVine
+	) as iv;
+
+
 --SELECT * FROM Deliverable2_S01_TotalVotes;
 --SELECT * FROM Deliverable2_S02_HelpfulPct;
 --SELECT * FROM Deliverable2_S03_IsVine;
-
-SELECT Count_TotalReviews, Count_All5Star, 
-		Count_Vine5Star, Cast(Count_Vine5Star / Count_All5Star as numeric(10,4)) as Pct_Vine5Star, 
-		Count_NonVine5Star, cast(Count_NonVine5Star / Count_All5Star as numeric(10,4)) as Pct_NonVine5Star
-INTO	Deliverable2_S05_Analysis
-FROM (
-	SELECT COUNT(review_id) as Count_TotalReviews
-	FROM Deliverable2_S02_HelpfulPct
-	) as ii CROSS JOIN (
-	SELECT cast(COUNT(review_id) as float) as Count_All5Star
-	FROM Deliverable2_S02_HelpfulPct
-	WHERE star_rating = 5
-	) as iii CROSS JOIN (
-	SELECT Cast(COUNT(review_id) as float) as Count_Vine5Star
-	FROM Deliverable2_S03_IsVine
-	WHERE star_rating = 5
-	) as iv CROSS JOIN (
-	SELECT cast(COUNT(review_id) as float) as Count_NonVine5Star
-	FROM Deliverable2_S04_NotVine
-	WHERE star_rating = 5
-	) as v
-
-
-
-
-
-
-
-
-
-
+--SELECT * FROM Deliverable2_S04_NotVine;
+SELECT * FROM Deliverable2_S05_Analysis;
 
 
